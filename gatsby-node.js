b1a5c57050016710c897c4baec38b8a791b4b9a3
Umut Tufanoglu <28development@gmail.com>
2021-02-17 14:49:34 +0100

    Initial commit.

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+import { Gitlab } from '@gitbeaker/node';
+
+exports.onPreInit = () => console.log("Loaded gatsby-source-gitlab-api")
+
+exports.sourceNodes = async ({ 
+    actions,
+    configOptions
+  }) => {
+
+    if (!configOptions.accessToken) {
+      throw 'You need to enter an accessToken';
+    }
+  
+    const { createNode } = actions
+
+    const api = new Gitlab({
+        token: configOptions.accessToken,
+    });
+
+    const projects = await api.Projects.all();
+
+    projects.forEach(project =>
+        createNode({
+            ...project,
+        })
+    )
+
+    return
+};
